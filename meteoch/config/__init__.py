3b399343afce30d1ffff48fccf3a1d47a94af4b3
Dominique Stussi <dominique.stussi@meteoswiss.ch>
2024-02-09 10:43:44 +0100

    refactor config file init

--- meteoch/config/__init__.py
+++ meteoch/config/__init__.py
@@ -1,23 +1,21 @@
 import os
 
-# if python version < 3.11, use tomli insatead of tomllib
+# if python version < 3.11, use tomli instead of tomllib
 try:
     import tomllib
 except ModuleNotFoundError:
     import tomli as tomllib
 
+# list of config files as value and variable name as key
+config = {'STATIONS' : 'stations.toml',
+          'MODELS_DET' : 'models_det.toml',
+          'MODELS_ENS' : 'models_ens.toml',
+          'PARAMETERS' : 'parameters.toml',
+          'PRODUCTS' : 'products.toml'
+          }
 
 # parse config files
-with open('/'.join([os.path.realpath(os.path.dirname(__file__)),"stations.toml"]), mode='rb') as fp:
-    STATIONS = tomllib.load(fp)
-
-with open('/'.join([os.path.realpath(os.path.dirname(__file__)),"models_det.toml"]), mode='rb') as fp:
-    MODELS_DET = tomllib.load(fp)
-
-with open('/'.join([os.path.realpath(os.path.dirname(__file__)),"models_ens.toml"]), mode='rb') as fp:
-    MODELS_ENS = tomllib.load(fp)
-    
-with open('/'.join([os.path.realpath(os.path.dirname(__file__)),"parameters.toml"]), mode='rb') as fp:
-    PARAMETERS = tomllib.load(fp)
-
-
+for key in config.keys():
+    print(f"Parsing {config[key]}")
+    with open('/'.join([os.path.realpath(os.path.dirname(__file__)),config[key]]), mode='rb') as fp:
+        globals()[key] = tomllib.load(fp)
